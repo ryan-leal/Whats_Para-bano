f7f44eb2034ef86bc7b1bd52688b647529e499c3
Ryan Leal <52039881+ryanLeal@users.noreply.github.com>
2019-09-12 07:44:34 -0300

    Função para Exibir a lista de amigos

--- Exibir_Amigos.c
+++ Exibir_Amigos.c
@@ -1,10 +1,11 @@
 //Função que exibe a lista de amigos, sendo passado como parâmetro o número de contatos salvos até o momento.
-void exibirAmigos(int i){
+void exibirAmigos(int nContatos){
   int aux;
+  printf("\e[H\e[2J");
   printf ("***********************LISTA DE AMIGOS*************************");
-  for (aux = 0;aux < i;aux++) {
+  for (aux = 0;aux < nContatos;aux++) {
       printf ("[%d]%s",aux,_contatos[aux].nome);
       printf ("[%d]%s\n",aux,_contatos[aux].IP);
   }
-
+  
 }
