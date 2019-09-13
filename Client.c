481e1921c45c28b514065f0834241913247ebaec
Ryan Leal <52039881+ryanLeal@users.noreply.github.com>
2019-09-13 11:08:15 -0300

    Updates_Ryan 0.1

--- Client.c
+++ Client.c
@@ -1,14 +1,20 @@
-
 #include <stdio.h>
 #include <string.h>
-#include <locale.h>
 #include <stdlib.h>
+#include <locale.h>
+
 /* Bibliotecas usadas para criar a conexão usando sockets 
 	e enviar dados pela rede*/
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
+//Struct do contato
+typedef struct {
+  char nome[30];
+  char IP[17];
+} tContato;
 
 /* Constante que define o tamanho máximo da mensagem enviada ou recebida*/
 #define TAM_MAX 100
@@ -17,81 +23,15 @@
 
 /* Variáveis globais usadas na conexão, envio e recebimento de dados*/
 static int sockfd, newFD;
-
-/*
-	Função que cria uma conexão com o cliente.
-	A função é bloqueante e fica esperando um pedido de conexão do cliente. 
-	Quando esse pedido é feito pelo cliente, se estiver tudo ok, a conexão é aceita. 
+	
+/**
+	Função que cria uma conexão com o servidor. 
+	Ele faz o pedido de conexão ao servidor e espera a resposta. 
+	IMPORTANTE: O servidor precisa estar rodando esperando o pedido de conexão para que a mesma seja realizada 
+	
+	Parametros: uma string contendo o endereço IP do servidor. 
 	Retorno: 1 se a conexão foi realizada com sucesso e -1 caso contrário.
  */
-int conecta(){
-	struct sockaddr_in myAddr;
-	struct sockaddr_in cliente;
-	
-	// Cria o socket
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if ( sockfd ==-1){
-		printf("Socket não pode ser criado\n");
-		return -1;
-	}
-
-	//Prepara a estrutura sockaddr_in 
-	myAddr.sin_family = AF_INET; 			// Protocolo IP
-	myAddr.sin_addr.s_addr = INADDR_ANY; 	// Permite receber conexoes de qualquer cliente
-	myAddr.sin_port = htons( PORTA ); 		// Porta utilizada na conexão
-     
-	//Bind
-	if(bind(sockfd,(struct sockaddr *)&myAddr , sizeof(myAddr)) < 0){
-    	printf("Bind falhou\n");
-	}
-	printf("Bind feito com sucesso\n");
-   
-    //Listen
-    listen(sockfd, 3);
-     
-    // Esperando e aceitando conexões
-    printf("Esperando por conexões...\n");
-    int c = sizeof(struct sockaddr_in);
-    newFD = accept(sockfd, (struct sockaddr *)&cliente, 
-    	(socklen_t*)&c);
-    
-    if (newFD<0){
-        printf("Conexão não aceita\n");
-    }
-    
-    printf("Conexão aceita de %s \n", inet_ntoa(cliente.sin_addr));
-    return 1;
-}
-
-/**
-	Envia uma mensagem (pacote) pela rede para o cliente conectado. 
-	Parametros: uma string com a mensagem a ser enviada e o tamanho (numero de bytes) dessa string. 
-	IMPORTANTE: A mensagem só será recebida se o cliente tiver chamada a função para receber a mensagem.
-	Retorno: o número de bytes enviados na mensagem
- */
-int enviaMensagem(char *msg, int tamMsg){
-	return send(newFD, msg, tamMsg, 0);
-}
-
-/**
-	Recebe uma mensagem (pacote) do cliente conectado. 
-	A função é bloqueante, e fica esperando o cliente enviar o pacote.
-	Parametros: uma string com a mensagem a ser enviada e o tamanho maximo de caracteres que podem ser preenchidos na mensagem. 
-	Retorno: número de bytes recebidos na mensagem
- */
-int recebeMensagem(char *msg, int tamMsg){
-	return recv(newFD, msg, tamMsg, 0);
-}
-
-
-
-
-
-//Struct do contato
-typedef struct {
-  char nome[30];
-  char IP[17];
-} tContato;
 
 //Varável/Array global dos contatos
 tContato _contatos[30];
@@ -99,6 +39,39 @@ tContato _contatos[30];
 //Variável global da posição
 int posicao = 0;
 
+//Funções Conexão
+int conecta(char *ipServidor){
+	struct sockaddr_in server;
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if ( sockfd ==-1){
+		printf("Socket não pode ser criado\n");
+	}
+
+	server.sin_addr.s_addr = inet_addr(ipServidor);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORTA);
+ 
+    //Connect to remote server
+    if (connect(sockfd, (struct sockaddr *)&server,sizeof(server)) < 0){
+        printf("Erro de conexão\n");
+        return -1;
+    }
+     
+    printf("Conectado\n");
+    return 1;
+}
+
+int enviaMensagem(char *msg, int tamMsg){
+	// Envia mensagem
+	return send(sockfd, msg, tamMsg, 0);
+}
+
+int recebeMensagem(char *msg, int tamMsg){
+	// Envia mensagem
+	return recv(sockfd, msg, tamMsg, 0);
+}
+
+//Funções da interface
 
 //Função com o número de contatos cadastrados
 int numeroContatos() {
@@ -116,7 +89,7 @@ int cadastrar(){
     int resposta;
 
     do{
-    system ("clear");
+    printf("\33[H\33[2J"); 
     printf ("_/*\\_ CADASTRO DE USUÁRIOS_/*\\_ \n\n");
     //Função para pesquisar um espaço do array vazio
     posicao = numeroContatos();
@@ -128,7 +101,7 @@ int cadastrar(){
     scanf ("%i", &resposta);
     getchar();
     } while (resposta != 0);
-    system ("clear");
+    printf("\33[H\33[2J");
   return 0;
 }
 
@@ -144,7 +117,6 @@ void exibirAmigos(int nContatos){
   }
 }
 
-//Função de Remoção
 void remover(tContato *amigos, int i, int numeroDeContatos){
     i++;
     
@@ -164,8 +136,6 @@ void remover(tContato *amigos, int i, int numeroDeContatos){
     strcpy(amigos[i].IP, stringNula);
   }
 
-
-//Menu de remoção
 void MenuRemocao(){
 
   printf("===================Remoção de contatos===================\n\n");
@@ -207,28 +177,35 @@ void MenuRemocao(){
 
 void conversar(){
   int contador = 0;
-  int ret;
-  int tamMsg;
-  char msgEnv[TAM_MAX];
-  char msgRec[TAM_MAX];
   int escolhaUsuario;
-  
-  do {
-      exibirAmigos(numeroContatos());
-      printf("\n \n Digite o número do contato que deseja se comunicar: ");
-      scanf("%i",&escolhaUsuario);
-      getchar();
-      system ("clear");	
-      ret = conecta();
-      system ("clear");
-      while (ret != -1){
-      		if (contador == 0) {
+  exibirAmigos(numeroContatos());
+  printf("\n \n Digite o número do contato que deseja se comunicar: ");
+  scanf("%i",&escolhaUsuario);
+  getchar();
+
+  //codigo do maritão
+  int ret;
+	int tamMsg;
+	char msgEnv[TAM_MAX];
+	char msgRec[TAM_MAX];
+
+	/* Pede para se conectar no servidor */
+	//ret	= conecta("150.165.201.14");
+  //ip previamente estabelecido para poder conversar com amigo
+  ret=conecta(_contatos[escolhaUsuario].IP);
+	if (ret != -1){
+		system("clear");
+		while (ret != -1) { 
+            if (contador == 0) {
             printf ("Bem-Vindo ao chat Digite /m para voltar ao menu ou /l para voltar para a lista de amigos");
             }
             contador++;
-            
-            
-        /* Lê uma mensagem do usuário e envia pela rede*/
+	
+	    /* Recebe uma mensagem pela rede */
+	    ret = recebeMensagem(msgRec, TAM_MAX);
+	    printf("Msg recebida: %s \n", msgRec);		
+
+	    /* Lê uma mensagem do usuário e envia pela rede*/
 	    printf("Digite a mensagem para enviar: ");
 	    fgets(msgEnv, TAM_MAX, stdin);
 	    tamMsg = strlen(msgEnv);
@@ -240,23 +217,13 @@ void conversar(){
 		
 	   ret = enviaMensagem(msgEnv, tamMsg);
 	   printf("Enviou uma mensagem com %d bytes\n", ret);
-	
-	    /* Recebe uma mensagem pela rede */
-	    ret = recebeMensagem(msgRec, TAM_MAX);
-	    printf("Msg recebida: %s \n", msgRec);		
 
-
-		
-		
-	  }
-	//Comparação para ficar na lista
-  }while((strcmp(msgEnv, "/m")!=0));
-  system ("clear");
-  
+		}	
+	}
 }
 
 int main(){
-	system ("clear");
+	system("clear");
     //Função para habilitar Acentos
     setlocale(LC_ALL, "Portuguese");
     int escolhaUsuario,escolha;
@@ -284,7 +251,7 @@ int main(){
         case 2:
           //função para conversar
           conversar();
-          system ("clear"); 
+          system ("clear");
           break;
         case 3:
           MenuRemocao();
